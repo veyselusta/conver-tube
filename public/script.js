6ad20d63ecbe8fb4471bf2f3819f281e72a8ccac
Veysel Usta <veyselusta7@gmail.com>
2021-01-15 21:57:55 +0300

    Edit ports to work locally

--- public/script.js
+++ public/script.js
@@ -1,11 +1,14 @@
 const button = document.querySelector('.convert-button')
 const URL = document.querySelector('.input')
 
+const PORT = 'covertube.herokuapp.com' || 'localhost:7000'
+
 button.addEventListener('click', ()=>{
   send(URL.value)
 })
 
 const send = (URL)=>{
-  window.location.href = `https://covertube.herokuapp.com/download?url=${URL}`
+  window.location.href = `https://${PORT}/download?url=${URL}`
 }
 
+http://localhost:3000/
