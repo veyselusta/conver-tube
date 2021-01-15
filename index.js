089f3a5b2a992e2c257d9f60bcdcb85ad7e9d128
Veysel Usta <veyselusta7@gmail.com>
2021-01-15 18:47:16 +0300

    Update views

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const indexRouter = require('./routes/index')
+const downloadRouter = require('./routes/download')
+const bodyParser = require('body-parser')
+const path = require('path');
+const PORT = process.env.PORT || 7000
+const app = express()
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug')
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json())
+
+
+app.use('/',indexRouter )
+app.use('/download', downloadRouter)
+
+app.listen(PORT, ()=>{
+  console.log('server şahlandı')
+})
