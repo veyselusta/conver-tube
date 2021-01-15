57ce9166c1f8b615c8f446629c02c2132310f1b5
Veysel Usta <veyselusta7@gmail.com>
2021-01-15 19:44:04 +0300

    Update script.js for deployment

--- public/script.js
+++ public/script.js
@@ -1,10 +1,11 @@
 const button = document.querySelector('.convert-button')
 const URL = document.querySelector('.input')
+const PORT = process.env.PORT || 7000
 
 button.addEventListener('click', ()=>{
   send(URL.value)
 })
 
 const send = (URL)=>{
-  window.location.href = `http://localhost:7000/download?url=${URL}`
+  window.location.href = `http://${PORT}/download?url=${URL}`
 }
