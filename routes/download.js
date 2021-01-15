157f96b885f5dd33f55b7cb534a445252a04cf2c
Veysel Usta <veyselusta7@gmail.com>
2021-01-15 20:07:41 +0300

    Fix typo

--- routes/download.js
+++ routes/download.js
@@ -3,11 +3,8 @@ const ytdl = require('ytdl-core')
 
 router.get('/', (req,res)=>{
   const url = req.query.url
-  res.header('Content-Disposition', 'attachment; filename = video.mp4')
+  res.header("Content-Disposition", 'attachment; filename="Video.mp4');
 
-  ytdl(url, {
-    format : 'mp4'
-  }).pipe(res)
-})
+  ytdl(url, {format : 'mp4'}).pipe(res)})
 
 module.exports = router
